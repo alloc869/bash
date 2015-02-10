92c5f8422257474780374edc6e8c9843bc3d9cdf
chunguang <chunguang.yang@windriver.com>
2015-02-10 10:30:39 +0800

    Add ioredirection basic script and testcaes
    basic io redirection script is added for do..done loop
    to learn how pipe and io redirection works.

---
+++ test.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if [ $# -lt 1 ]
+then
+	echo "usage: $0  filename " >&2
+fi
+count=1
+cat $1 | while read line
+do
+	[ $count -eq 1 ] && echo "Processing file $1..." >  /dev/tty
+	echo $count $line 
+	count=`expr $count + 1`
+done  > tmp$$
+echo $count
+mv tmp$$ tmp$$.log
